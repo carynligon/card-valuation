7735870b771a28bf4d7e3e1225dcd5d9d238f04c
Caryn Ligon <carynligon@caryns-air-2.lan>
2023-10-29 14:21:29 -0500

    add item

--- src/pages/auth/success/index.tsx
+++ src/pages/auth/success/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const getAuthToken = async (code: string) => {
   const params = new URLSearchParams();
@@ -11,41 +11,75 @@ const getAuthToken = async (code: string) => {
       `/api/authToken?code=${encodeURIComponent(code)}`
     );
     const localToken = await localRes.json();
-    console.log("localToken", localToken);
-    const res = await fetch(
-      "https://api.sandbox.ebay.com/identity/v1/oauth2/token",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-          authorization: `Basic ${btoa(
-            process.env.EBAY_CLIENT_ID + ":" + process.env.EBAY_CLIENT_SECRET
-          )}`,
-          response_type: "token",
-        },
-        body: params,
-      }
-    );
-    console.log("res", res);
-    const token = await res.json();
-    console.log("token", token);
-    return token.access_token;
+
+    return localToken.message;
   } catch (e) {
     console.log("error", e);
   }
 };
 
 const AuthSuccessPage = () => {
+  const [authToken, setAuthToken] = useState("");
+  const getToken = async (code: string) => {
+    const token = await getAuthToken(code);
+    setAuthToken(token);
+  };
   useEffect(() => {
     if (window && window.location) {
       const params = new URLSearchParams(window.location.search);
       const code = params.get("code") || "";
       console.log("got code", code);
-      getAuthToken(code);
+      getToken(code);
     }
   }, []);
 
-  return <>Success!</>;
+  const addItem = async () => {
+    const item = {
+      availability: {
+        shipToLocationAvailability: {
+          quantity: 50,
+        },
+      },
+      condition: "NEW",
+      product: {
+        title: "GoPro Hero4 Helmet Cam",
+        description: "New GoPro Hero4 Helmet Cam. Unopened box.",
+        aspects: {
+          Brand: ["GoPro"],
+          Type: ["Helmet/Action"],
+          "Storage Type": ["Removable"],
+          "Recording Definition": ["High Definition"],
+          "Media Format": ["Flash Drive (SSD)"],
+          "Optical Zoom": ["10x"],
+        },
+        brand: "GoPro",
+        mpn: "CHDHX-401",
+        imageUrls: [
+          "https://i*****g.com/0**********/**********1.jpg",
+          "https://i*****g.com/0**********/**********2.jpg",
+          "https://i*****g.com/0**********/**********3.jpg",
+        ],
+      },
+    };
+    const res = await fetch(
+      "https://api.sandbox.ebay.com/sell/inventory/v1/inventory_item/12345678",
+      {
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify(item),
+      }
+    );
+    const stuff = await res.json();
+    console.log("sell stuff", stuff);
+  };
+
+  return (
+    <>
+      <button onClick={addItem}>Add item</button>
+    </>
+  );
 };
 
 export default AuthSuccessPage;
