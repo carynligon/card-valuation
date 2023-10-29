d5e5055e8291196ac103633ba160eaac5d8aced5
Caryn Ligon <carynligon@caryns-air-2.lan>
2023-10-29 14:25:48 -0500

    add item api

---
+++ src/pages/api/addItem.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const item = {
+    availability: {
+      shipToLocationAvailability: {
+        quantity: 50,
+      },
+    },
+    condition: "NEW",
+    product: {
+      title: "GoPro Hero4 Helmet Cam",
+      description: "New GoPro Hero4 Helmet Cam. Unopened box.",
+      aspects: {
+        Brand: ["GoPro"],
+        Type: ["Helmet/Action"],
+        "Storage Type": ["Removable"],
+        "Recording Definition": ["High Definition"],
+        "Media Format": ["Flash Drive (SSD)"],
+        "Optical Zoom": ["10x"],
+      },
+      brand: "GoPro",
+      mpn: "CHDHX-401",
+      imageUrls: [
+        "https://i*****g.com/0**********/**********1.jpg",
+        "https://i*****g.com/0**********/**********2.jpg",
+        "https://i*****g.com/0**********/**********3.jpg",
+      ],
+    },
+  };
+  console.log("REQ", req.query);
+  const authToken = decodeURIComponent(req.query.token as string);
+  const params = new URLSearchParams();
+  const ebayRes = await fetch(
+    "https://api.sandbox.ebay.com/sell/inventory/v1/inventory_item/12345678",
+    {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(item),
+    }
+  );
+  const stuff = await ebayRes.json();
+  console.log("sell stuff", stuff);
+
+  res.status(200).json({ message: "item added!" });
+}
--- src/pages/auth/success/index.tsx
+++ src/pages/auth/success/index.tsx
@@ -34,45 +34,9 @@ const AuthSuccessPage = () => {
   }, []);
 
   const addItem = async () => {
-    const item = {
-      availability: {
-        shipToLocationAvailability: {
-          quantity: 50,
-        },
-      },
-      condition: "NEW",
-      product: {
-        title: "GoPro Hero4 Helmet Cam",
-        description: "New GoPro Hero4 Helmet Cam. Unopened box.",
-        aspects: {
-          Brand: ["GoPro"],
-          Type: ["Helmet/Action"],
-          "Storage Type": ["Removable"],
-          "Recording Definition": ["High Definition"],
-          "Media Format": ["Flash Drive (SSD)"],
-          "Optical Zoom": ["10x"],
-        },
-        brand: "GoPro",
-        mpn: "CHDHX-401",
-        imageUrls: [
-          "https://i*****g.com/0**********/**********1.jpg",
-          "https://i*****g.com/0**********/**********2.jpg",
-          "https://i*****g.com/0**********/**********3.jpg",
-        ],
-      },
-    };
-    const res = await fetch(
-      "https://api.sandbox.ebay.com/sell/inventory/v1/inventory_item/12345678",
-      {
-        method: "PUT",
-        headers: {
-          authorization: `Bearer ${authToken}`,
-        },
-        body: JSON.stringify(item),
-      }
-    );
-    const stuff = await res.json();
-    console.log("sell stuff", stuff);
+    const localRes = await fetch(`/api/addItem?token=${authToken}}`);
+    const localItem = await localRes.json();
+    console.log("localItem", localItem);
   };
 
   return (
