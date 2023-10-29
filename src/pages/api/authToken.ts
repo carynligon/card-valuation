c6578d50699597e3c0fdac973f91b6095e47c62a
Caryn Ligon <carynligon@caryns-air-2.lan>
2023-10-29 14:03:07 -0500

    oops

--- src/pages/api/authToken.ts
+++ src/pages/api/authToken.ts
@@ -10,37 +10,28 @@ export default async function handler(
 ) {
   let token = "";
   const code = req.query.code || "";
-  const getAuthToken = async () => {
-    const params = new URLSearchParams();
-    params.append("grant_type", "authorization_code");
-    params.append("redirect_uri", "Caryn_Ligon-CarynLig-cardva-aydfzye");
-    params.append("code", code as string);
-    try {
-      const res = await fetch(
-        "https://api.sandbox.ebay.com/identity/v1/oauth2/token",
-        {
-          method: "POST",
-          headers: {
-            "content-type": "application/x-www-form-urlencoded",
-            authorization: `Basic ${btoa(
-              process.env.EBAY_CLIENT_ID + ":" + process.env.EBAY_CLIENT_SECRET
-            )}`,
-            response_type: "token",
-          },
-          body: params,
-        }
-      );
-      console.log("res222", res);
-      const tokenResp = await res.json();
-      console.log("kkkkk", tokenResp);
-      token = tokenResp.access_token;
-    } catch (e) {
-      res.status(500).json({ message: JSON.stringify(e) });
-      console.log("error", e);
+  const params = new URLSearchParams();
+  params.append("grant_type", "authorization_code");
+  params.append("redirect_uri", "Caryn_Ligon-CarynLig-cardva-aydfzye");
+  params.append("code", code as string);
+  const ebayRes = await fetch(
+    "https://api.sandbox.ebay.com/identity/v1/oauth2/token",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        authorization: `Basic ${btoa(
+          process.env.EBAY_CLIENT_ID + ":" + process.env.EBAY_CLIENT_SECRET
+        )}`,
+        response_type: "token",
+      },
+      body: params,
     }
-  };
-
-  await getAuthToken();
+  );
+  console.log("res222", ebayRes);
+  const tokenResp = await ebayRes.json();
+  console.log("kkkkk", tokenResp);
+  token = tokenResp.access_token;
 
   res.status(200).json({ message: token });
 }
