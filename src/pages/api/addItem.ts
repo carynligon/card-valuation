b6bf2910b9da0ab61072a4537257628c9cbeffcf
Caryn Ligon <carynligon@caryns-air-2.lan>
2023-10-30 16:34:21 -0500

    item header

--- src/pages/api/addItem.ts
+++ src/pages/api/addItem.ts
@@ -75,6 +75,7 @@ export default async function handler(
         headers: {
           authorization: `Bearer ${authToken}`,
           "content-type": "application/json",
+          "Content-Language": "en-US",
         },
         body: JSON.stringify(policy),
       }
